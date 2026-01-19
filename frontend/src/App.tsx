1c7b8f96b726f40fb376d5cffffe75c6b2999a39
lrv491 <lucia.ramirez491@comunidadunir.net>
2026-01-19 12:28:18 +0100

    Revert "UPDATE: Se han añadido rutas con componentes, modificado main.tsx y m…"

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,13 @@
 import { useEffect, useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { RegisterView } from "./routes/RegisterView";
-import { auth } from "./firebase/firebase";
 
 function App() {
   const [data, setData] = useState<any>(null);
-  const user = auth.currentUser;
 
   useEffect(() => {
     // 🔹 Llamada directa al backend
     async function fetchData() {
       try {
-        const res = await fetch("http://localhost:3000/otro-endpoint");
+        const res = await fetch("http://localhost:3000/test");
         const json = await res.json();
         setData(json);
       } catch (err) {
@@ -23,13 +19,10 @@ function App() {
   }, []);
 
   return (
-    <>
     <div>
       <h1>Prueba Frontend → Backend</h1>
       <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
-    </>
-    
   );
 }
 
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,34 +1,10 @@
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { RegisterView } from './routes/RegisterView.tsx'
-import Login from './routes/LoginView.tsx'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { PaginaNoExistente } from './routes/PaginaNoExistente.tsx'
-import { AuthLayout } from './layouts/AuthLayout.tsx'
-import { MiCuentaView } from './routes/MiCuentaView.tsx'
-import { MenuView } from './routes/MenuView.tsx'
-import { AuthStatus } from './hooks/AuthStatus.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <Routes>
-      <Route element={<AuthStatus />}>
-        <Route element={<MenuView />}>
-          {/*Ruta Principal*/}
-          <Route index element={<App />} />
-          <Route path=':nombreUsuario' element={<MiCuentaView />} />
-        </Route>
-      </Route>
-
-
-      <Route element={<AuthLayout />}>
-        <Route path='login' element={<Login />} />
-        <Route path='registro' element={<RegisterView />} />
-      </Route>
-
-      {/* Rutas no validas */}
-      <Route path='*' element={<PaginaNoExistente />} />
-    </Routes>
-  </BrowserRouter >,
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
