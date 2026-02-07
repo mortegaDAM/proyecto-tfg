527844c371c7458825fa3065bc34da2e93d06357
lrv491 <lucia.ramirez491@comunidadunir.net>
2026-02-07 13:26:33 +0100

    Update: Se crearon las clases pertenecientes a Puesto y se realizaron los endpoints principales de todos

---
+++ backend/src/main/java/com/dando_la_vez/backend/model/Puesto.java
@@ -0,0 +1,54 @@
+package com.dando_la_vez.backend.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "puestos")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Puesto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column
+    private String nombre;
+    @Column
+    private boolean abierto;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_usuario")
+    @JsonBackReference("puesto-usuario")
+    private Usuario usuario;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_mercado")
+    @JsonBackReference("puesto-mercado")
+    private Mercado mercado;
+
+    @JsonBackReference("cliente-turno-puesto")
+    @ManyToMany(mappedBy = "listaPuestos")
+    public Set<Cliente> listaClientes;
+
+
+    public Puesto(String nombre, boolean abierto) {
+        this.nombre = nombre;
+        this.abierto = abierto;
+    }
+
+    public Puesto(String nombre, boolean abierto, Usuario usuario, Mercado mercado) {
+        this.nombre = nombre;
+        this.abierto = abierto;
+        this.usuario = usuario;
+        this.mercado = mercado;
+    }
+}
